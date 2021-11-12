86fb688f634d3fefbf77939a5c129c827f855659
Russel Arbore <russel.jma@gmail.com>
2021-11-11 21:51:52 -0600

    Made recursive exprs ptrs, destructors

--- src/expr.cpp
+++ src/expr.cpp
@@ -1,14 +1,14 @@
 #include "expr.hpp"
 
-Logical::Logical(Expr left, Token op, Expr right): left(left), op(op), right(right) {}
+Expr::~Expr() {}
 
-Assign::Assign(Token name, Expr value): name(name), value(value){}
+Assign::Assign(Token name, Expr* value): name(name), value(value) {}
 
-Binary::Binary(Expr left, Token op, Expr right): left(left), op(op), right(right) {}
+Binary::Binary(Expr* left, Token op, Expr* right): left(left), op(op), right(right) {}
 
-Func::Func(Token func, Token paren, std::vector<Expr> args): func(func), paren(paren), args(args) {}
+Func::Func(Token func, Token paren, std::vector<Expr*> args): func(func), paren(paren), args(args) {}
 
-Group::Group(Expr expr): expr(expr) {}
+Group::Group(Expr* expr): expr(expr) {}
 
 Literal::Literal(std::string val): string_val(val) {}
 
@@ -16,10 +16,48 @@ Literal::Literal(double val): double_val(val) {}
 
 Literal::Literal(bool val): bool_val(val) {}
 
-Logical::Logical(Expr left, Token op, Expr right): left(left), op(op), right(right) {}
+Logical::Logical(Expr* left, Token op, Expr* right): left(left), op(op), right(right) {}
 
-Op::Op(Token op, Token paren, std::vector<Expr> args): op(op), paren(paren), args(args) {}
+Op::Op(Token op, Token paren, std::vector<Expr*> args): op(op), paren(paren), args(args) {}
 
-Unary::Unary(Token op, Expr right): op(op), right(right) {}
+Unary::Unary(Token op, Expr* right): op(op), right(right) {}
 
 Var::Var(Token name): name(name) {}
+
+Assign::~Assign() {
+    delete value;
+}
+
+Binary::~Binary() {
+    delete left;
+    delete right;
+}
+
+Func::~Func() {
+    for (auto ptr : args) {
+        delete ptr;
+    }
+}
+
+Group::~Group() {
+    delete expr;
+}
+
+Literal::~Literal() {}
+
+Logical::~Logical() {
+    delete left;
+    delete right;
+}
+
+Op::~Op() {
+    for (auto ptr : args) {
+        delete ptr;
+    }
+}
+
+Unary::~Unary() {
+    delete right;
+}
+
+Var::~Var() {}
