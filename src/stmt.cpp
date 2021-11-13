d5baffd7d7ae254918edc36365f3f610bc30152e
Russel Arbore <russel.jma@gmail.com>
2021-11-13 09:53:29 -0600

    Finished stmt construcotrs

--- src/stmt.cpp
+++ src/stmt.cpp
@@ -2,23 +2,23 @@
 
 Stmt::~Stmt();
 
-Block::Block(std::vector<Stmt*> stmts) {}
+Block::Block(std::vector<Stmt*> stmts): stmts(stmts) {}
 
-ExprStmt::ExprStmt(Expr* expr) {}
+ExprStmt::ExprStmt(Expr* expr): expr(expr) {}
 
-FuncDecl::FuncDecl(Token name, std::vector<Token> params, std::vector<Stmt*> stmts) {}
+FuncDecl::FuncDecl(Token name, std::vector<Token> params, std::vector<Stmt*> stmts): name(name), params(params), stmts(stmts) {}
 
-If::If(Expr* cond, Stmt* pos_branch, Stmt* neg_branch) {}
+If::If(Expr* cond, Stmt* pos_branch, Stmt* neg_branch): cond(cond), pos_branch(pos_branch), neg_branch(neg_branch) {}
 
-OpDecl::OpDecl(Token name, Token left, Token right, std::vector<Stmt*> stmts) {}
+OpDecl::OpDecl(Token name, Token left, Token right, std::vector<Stmt*> stmts): name(name), left(left), right(right), stmts(stmts) {}
 
-Print::Print(Expr* expr) {}
+Print::Print(Expr* expr): expr(expr) {}
 
-Return::Return(Token return_keyword, Expr* expr) {}
+Return::Return(Token return_keyword, Expr* expr): return_keyword(return_keyword), expr(expr) {}
 
-VarDecl::VarDecl(Token name, Expr* expr) {}
+VarDecl::VarDecl(Token name, Expr* expr): name(name), expr(expr) {}
 
-While::While(Expr* cond, Stmt* body) {}
+While::While(Expr* cond, Stmt* body): cond(cond), body(body) {}
 
 Block::~Block() {
     for (auto stmt : stmts) delete stmt;
