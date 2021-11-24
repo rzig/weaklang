e52e965892bce84ad1a58bf8163ebd6d17b8eab9
Russel Arbore <russel.jma@gmail.com>
2021-11-24 12:08:41 -0600

    Basic arith opers eval

--- include/environment.hpp
+++ include/environment.hpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <stdexcept>
 #include <string>
+#include <math.h>
 
 #include "variable.hpp"
 #include "parser.hpp"
@@ -13,6 +14,36 @@
 #define FUNC_EXISTS(func) (func_symbol_table.find(func) != func_symbol_table.end())
 #define OP_EXISTS(op) (op_symbol_table.find(op) != op_symbol_table.end())
 #define VAR_EXISTS(var) (var_symbol_table.find(var) != var_symbol_table.end())
+
+#define ELEMENTWISE_OP(OP) { \
+    if (left_var.is_double() && right_var.is_double()) { \
+	return Variable(std::get<double>(left_var.value) OP std::get<double>(right_var.value)); \
+    } \
+    if (left_var.is_double() && right_var.is_ndarray()) { \
+	auto right_arr = std::get<std::pair<std::vector<double>, std::vector<size_t>>>(right_var.value).first; \
+	for (size_t i = 0; i < right_arr.size(); i++) { \
+	    right_arr.at(i) = std::get<double>(left_var.value) OP right_arr.at(i); \
+	} \
+	return Variable(std::pair<std::vector<double>, std::vector<size_t>>(right_arr, std::get<std::pair<std::vector<double>, std::vector<size_t>>>(right_var.value).second)); \
+    } \
+    if (left_var.is_ndarray() && right_var.is_double()) { \
+	auto left_arr = std::get<std::pair<std::vector<double>, std::vector<size_t>>>(left_var.value).first; \
+	for (size_t i = 0; i < left_arr.size(); i++) { \
+	    left_arr.at(i) = left_arr.at(i) OP std::get<double>(right_var.value); \
+	} \
+	return Variable(std::pair<std::vector<double>, std::vector<size_t>>(left_arr, std::get<std::pair<std::vector<double>, std::vector<size_t>>>(left_var.value).second)); \
+    } \
+    if (left_var.is_ndarray() && right_var.is_ndarray()) { \
+	auto left_var_pair = std::get<std::pair<std::vector<double>, std::vector<size_t>>>(left_var.value); \
+	auto right_var_pair = std::get<std::pair<std::vector<double>, std::vector<size_t>>>(right_var.value); \
+	runtime_assert(left_var_pair.second == right_var_pair.second, binary->op, "Expressions evaluate to arrays of differing sizes"); \
+	std::vector<double> zipped; \
+	for (size_t i = 0; i < left_var_pair.first.size(); i++) { \
+	    zipped.push_back(left_var_pair.first.at(i) OP right_var_pair.first.at(i)); \
+	} \
+	return Variable(std::pair<std::vector<double>, std::vector<size_t>>(zipped, left_var_pair.second)); \
+    } \
+}
 
 class Environment {
 public:
