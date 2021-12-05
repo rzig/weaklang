aaefd9010d309bd8c2574fb80e5810e30c6ebb0f
Ryan Ziegler <rzig408@gmail.com>
2021-12-05 11:48:16 -0600

    create clean playground UI

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
-import Editor from "@monaco-editor/react";
+import Editor, { useMonaco } from "@monaco-editor/react";
 
 function App() {
   const editor = useRef<any>();
 
   const [output, setOutput] = useState("");
-
-  useEffect(() => {
-  }, []);
 
   const run = () => {
     const program = editor.current.getValue();
@@ -31,31 +28,45 @@ function App() {
   };
 
   return (
-    <>
-      <nav style={{display: "flex", flexDirection: "row", alignContent: "center", alignItems: "center", justifyContent: "center", height: "10vh", backgroundColor: "#ccc"}}>
-        <div>
-          <button style={{display: "block"}} onClick={run}>Run Code</button>
+    <div style={{height: "100vh", display: "flex", flexDirection: "column", maxHeight: "100vh"}}>
+      <nav style={{display: "flex", flexDirection: "row", alignContent: "center", alignItems: "center", justifyContent: "space-between", height: "10vh", backgroundColor: "#EDEDED", paddingLeft: "30px", paddingRight: "30px"}}>
+        <div style={{width: 300, display: "flex", flexGrow: 1, alignItems: "center"}}>
+          <h1 style={{fontFamily: "'Inter', sans-serif", fontSize: "1.6rem"}}>WeakLang Playground</h1>
+        </div>
+        <div style={{display: "flex"}}>
+          <a style={{display: "block", backgroundColor: "#81d4fa ", borderColor: "#03a9f4", borderStyle: "solid", borderWidth: "2px", borderRadius: "5px", padding: "16px", cursor: "pointer", fontFamily: "'Inter', sans-serif", fontSize: "1.15rem", marginRight: "15px", textDecoration: "none", color: "black"}} onClick={run} href="https://github.com/rzig/weaklang">See Github</a>
+          <button style={{display: "block", backgroundColor: "#2BFFA7", borderColor: "#21BF7D", borderStyle: "solid", borderWidth: "2px", borderRadius: "5px", padding: "16px", cursor: "pointer", fontFamily: "'Inter', sans-serif", fontSize: "1.15rem"}} onClick={run}>Run Code</button>
         </div>
       </nav>
-      <div style={{display: "flex", flexDirection: "row"}}>
-        <Editor
-          height="90vh"
-          width="50vw"
-          defaultLanguage="plaintext"
-          defaultValue="//Weak"
-          onMount={(editor_b, monaco) => {
-            editor.current = editor_b;
-            console.log("editor.current is " + editor.current)
-          }}
-        />
-        <Editor
-          height="90vh"
-          width="50vw"
-          defaultLanguage="plaintext"
-          value={output}
-        />
+      <div style={{flexGrow: 1, height: "100%"}}>
+        <div style={{width: "100%", height: "100%"}}>
+          <div style={{display: "flex", flexDirection: "row", padding: "30px", height: "90%"}}>
+            <div style={{borderRadius: 7, border: "1px solid #DEDEDE", height: "100%", flexGrow: 1, marginRight: "30px", width: "50%", overflow: "hidden"}}>
+              <div style={{width: "100%", height: "100%"}}>
+                <Editor
+                  defaultLanguage="plaintext"
+                  defaultValue="p T;"
+                  onMount={(editor_b, monaco_b) => {
+                    editor.current = editor_b;
+                  }}
+                  options={{minimap: {enabled: false}, automaticLayout: true, fontFamily: "Roboto Mono", fontSize: 16}}
+                />
+              </div>
+            </div>
+
+            <div style={{borderRadius: 7, border: "1px solid #DEDEDE", height: "100%", flexGrow: 1, width: "50%", overflow: "hidden"}}>
+              <div style={{width: "100%", height: "100%"}}>
+                <Editor
+                  defaultLanguage="plaintext"
+                  value={output}
+                  options={{minimap: {enabled: false}, readOnly: true, fontFamily: "Roboto Mono", fontSize: 16}}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
